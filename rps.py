592dd6f6ce8cc818b75ec12e4d0b3807bbe74ba5
[aidsemancik] <[asemancik14@gmail.com]>
2023-03-08 14:53:15 -0600

    made it user friendly

--- rps.py
+++ rps.py
@@ -1,45 +1,64 @@
 import random
 
-actions = ["Rock", "Paper", "Scissors"]
+def rps():
+    # Computer Options
+    actions = ["r", "p", "s"]
 
-user_points = 0
-comp_points = 0
+    # User's and Computer's Points Variables
+    user_points = 0
+    comp_points = 0
 
-rounds = 1
+    # Rounds
+    rounds = 1
 
-while rounds <= 3:
-    user_guess = input("Rock, Paper, Scissors!")
+    # Introduction - asks user what their name is
+    name = input("Today we will be playing Rock, Paper, Scissors. What's your name? ")
+    print("Welcome " + name + "! Let's get started!!")
 
-    computer_guess = random.choice(actions)
+    # Will play for 3 rounds unless a tie
+    while rounds <= 3:
+        # Will ask the player what they want to play
+        user_guess = input("Rock, Paper, Scissors! (r, p, s): ")
 
-    rounds += 1
+        # The computer will decide what it wants to play
+        computer_guess = random.choice(actions)
 
-    if user_guess == computer_guess:
-        print("Oops! We guessed the same number. Lets go again!")
-        rounds -= 1
-        continue
-    elif user_guess == "Rock" and computer_guess == "Scissors":
-        user_points += 1
-        print("Haha! Rock beats Scissors, point for user!")
-    elif user_guess == "Paper" and computer_guess == "Rock":
-        user_points += 1
-        print("Haha! Paper beats Rock, point for user!")
-    elif user_guess == "Scissors" and computer_guess == "Paper":
-        user_points += 1
-        print("Haha! Scissors beats Paper, point for user!")
-    elif user_guess == "Scissors" and computer_guess == "Rock":
-        comp_points += 1
-        print("Aww... Rock beats Scissors, point for computer!")
-    elif user_guess == "Rock" and computer_guess == "Paper":
-        comp_points += 1
-        print("Aww... Paper beats Rock, point for computer!")
-    elif user_guess == "Paper" and computer_guess == "Scissors":
-        comp_points += 1
-        print("Aww... Scissors beats Paper, point for computer!")
-    
-if user_points > comp_points:
-    print("YAY! We won with " + str(user_points) + " points!")
-else:
-    print("Aww we lost to the computer... we had " + str(user_points) + " points and the computer had " + str(comp_points) + " points.")
+        # Adds a round being played
+        rounds += 1
 
-# commit1
+        if user_guess == computer_guess:
+            print("Oops! We guessed the same move. Lets go again!")
+            rounds -= 1
+            continue
+        elif user_guess == "r" and computer_guess == "s":
+            user_points += 1
+            print("Haha! Rock beats Scissors, point for " + name + "!")
+        elif user_guess == "p" and computer_guess == "r":
+            user_points += 1
+            print("Haha! Paper beats Rock, point for " + name + "!")
+        elif user_guess == "s" and computer_guess == "p":
+            user_points += 1
+            print("Haha! Scissors beats Paper, point for " + name + "!")
+        elif user_guess == "s" and computer_guess == "r":
+            comp_points += 1
+            print("Aww... Rock beats Scissors, point for computer!")
+        elif user_guess == "r" and computer_guess == "p":
+            comp_points += 1
+            print("Aww... Paper beats Rock, point for computer!")
+        elif user_guess == "p" and computer_guess == "s":
+            comp_points += 1
+            print("Aww... Scissors beats Paper, point for computer!")
+        
+    if user_points > comp_points:
+        print("YAY! " + name + " won with " + str(user_points) + " points! The computer had " + str(comp_points) + " points.")
+    else:
+        print("Aww " + name + " lost to the computer... we only had " + str(user_points) + " points and the computer had " + str(comp_points) + " points.")
+
+    play = input("Want to play again? y/n: ")
+    if play == "y":
+        rps()
+    else:
+        print("We'll play again another time. Bye Bye!")
+
+rps()
+
