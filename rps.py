1c66da1c4b357e15c6d3403fae9c9f383cd68d24
aidsemancik <asemancik14@gmail.com>
2023-03-08 22:08:57 -0600

    added more pseudo code, and brief notes for presentation

--- rps.py
+++ rps.py
@@ -1,31 +1,32 @@
 import random
 
 def rps():
-    # Computer Options
+    # List of computer Options
     actions = ["r", "p", "s"]
 
-    # User's and Computer's Points Variables
+    # Variables for user's and computer's Points
     user_points = 0
     comp_points = 0
 
-    # Rounds
+    # Variable for number of rounds
     rounds = 1
 
     # Introduction - asks user what their name is
     name = input("Today we will be playing Rock, Paper, Scissors. What's your name? ")
     print("Welcome " + name + "! Let's get started!!")
 
-    # Will play for 3 rounds unless a tie
+    # While the rounds are less than or equal to 3 the game will be played
     while rounds <= 3:
-        # Will ask the player what they want to play
+        # User input to ask what their move is
         user_guess = input("Rock, Paper, Scissors! (r, p, s): ")
 
-        # The computer will decide what it wants to play
+        # Random function to allow the computer to pick a move
         computer_guess = random.choice(actions)
 
-        # Adds a round being played
+        # Adds a round played to the rounds variable
         rounds += 1
 
+        # If/Else statement to determine whether the user or computer receives a point
         if user_guess == computer_guess:
             print("Oops! We guessed the same move. Lets go again!")
             rounds -= 1
@@ -48,17 +49,30 @@ def rps():
         elif user_guess == "p" and computer_guess == "s":
             comp_points += 1
             print("Aww... Scissors beats Paper, point for computer!")
-        
+
+    # If/Else statement to determines if the user won or lost 
     if user_points > comp_points:
         print("YAY! " + name + " won with " + str(user_points) + " points! The computer had " + str(comp_points) + " points.")
     else:
         print("Aww " + name + " lost to the computer... we only had " + str(user_points) + " points and the computer had " + str(comp_points) + " points.")
 
+    # Asks the user if they would like to play again with an If/Else statement
     play = input("Want to play again? y/n: ")
     if play == "y":
         rps()
     else:
         print("We'll play again another time. Bye Bye!")
 
+# Starts the game
 rps()
+
+
+
+
+# Clear Terminal
+# 1. nano ~/.bash_profile
+# 2. export PS1="\$ "
+# 3. source ~/.bash_profile
+# Use the source command every time terminal is started, undo changes once complete
+
 
